8b94c8115c99ae4fd4c8d3da952138ede0d5a623
huangfj <ops@speakin.ai>
2022-04-12 11:32:07 +0800

    add Dockerfile

--- microservice/microservice/settings.py
+++ microservice/microservice/settings.py
@@ -79,9 +79,15 @@ WSGI_APPLICATION = 'microservice.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'speakin_root',
+        'HOST': '192.168.21.101',
+        'PORT': 3306,
+        'NAME': 'microservice',
+        'TEST': {
+            'NAME': 'microservice',
+        },
 }
 
 
